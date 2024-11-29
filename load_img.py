e6d00d6e3664c318cfe3804e5929f3fcfb216465
phamhungd <phamhung.d@gmail.com>
2024-11-29 22:12:23 +0700

    update

--- load_img.py
+++ load_img.py
@@ -1,8 +1,12 @@
 import requests
+import os
 from PIL import Image, ImageOps
 import torch
 import subprocess
 import numpy as np
+import folder_paths
+import hashlib
+from PIL.PngImagePlugin import PngInfo
 
 
 def pil2tensor(images: Image.Image | list[Image.Image]) -> torch.Tensor:
@@ -60,13 +64,84 @@ class load_image_url:
         return (pil2tensor(image),)
 
 
+class LoadImage:
+    @classmethod
+    def INPUT_TYPES(s):
+        input_dir = folder_paths.get_input_directory()
+        exclude_folders = ["clipspace", "folder_to_exclude2"]
+        file_list = []
+
+        for root, dirs, files in os.walk(input_dir):
+            # Exclude specific folders
+            dirs[:] = [d for d in dirs if d not in exclude_folders]
+
+            for file in files:
+                file_path = os.path.relpath(
+                    os.path.join(root, file), start=input_dir)
+                # so the filename is processed correctly in widgets.js
+                file_path = file_path.replace("\\", "/")
+                file_list.append(file_path)
+
+        return {"required": {
+            "Load_url": ("BOOLEAN", {"default": True},),
+            "Url": ("STRING", {"default": "", "multiline": False},),
+            "image": (sorted(file_list), {"image_upload": True})
+
+        }
+        }
+
+    CATEGORY = "SDVN"
+
+    RETURN_TYPES = ("IMAGE", "MASK")
+    FUNCTION = "load_image"
+
+    def load_image(self, image, Url, Load_url):
+        if Url != '' and Load_url:
+            if 'pinterest.com' in Url:
+                Url = run_gallery_dl(Url)
+            image = Image.open(requests.get(Url, stream=True).raw)
+            image = ImageOps.exif_transpose(image)
+            return (pil2tensor(image),)
+
+        else:
+            image_path = folder_paths.get_annotated_filepath(image)
+            i = Image.open(image_path)
+            i = ImageOps.exif_transpose(i)
+            image = i.convert("RGB")
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64, 64), dtype=torch.float32, device="cpu")
+            return (image, mask.unsqueeze(0))
+
+    @classmethod
+    def IS_CHANGED(s, image, Url, Load_url):
+        image_path = folder_paths.get_annotated_filepath(image)
+        m = hashlib.sha256()
+        with open(image_path, 'rb') as f:
+            m.update(f.read())
+        return m.digest().hex()
+
+    @classmethod
+    def VALIDATE_INPUTS(s, image, Url, Load_url):
+        if not folder_paths.exists_annotated_filepath(image):
+            return "Invalid image file: {}".format(image)
+
+        return True
+
+
 # A dictionary that contains all nodes you want to export with their names
 # NOTE: names should be globally unique
 NODE_CLASS_MAPPINGS = {
-    "Load Image Url": load_image_url
+    "Load Image Url": load_image_url,
+    "Load Image (SubFolder)": LoadImage
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "Load Image Url": "SDVN Load Image"
+    "Load Image Url": "SDVN Load Image Url",
+    "Load Image (SubFolder)": "SDVN Load Image"
 }
