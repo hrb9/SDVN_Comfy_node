8719146be6dbb659b632ebcae4f1218adb0edab3
phamhungd <phamhung.d@gmail.com>
2024-12-02 18:52:00 +0700

    Update

--- node/chatbot.py
+++ node/chatbot.py
@@ -1,4 +1,5 @@
 import google.generativeai as genai
+from openai import OpenAI
 from nodes import NODE_CLASS_MAPPINGS as ALL_NODE_CLASS_MAPPINGS
 
 
@@ -6,10 +7,11 @@ class API_chatbot:
     @classmethod
     def INPUT_TYPES(s):
         return {"required": {
-            "chatbot": (["Gemini 1.5 Flash", "Gemini 1.5 Pro"],),
+            "chatbot": (["Gemini 1.5 Flash", "Gemini 1.5 Pro", "HuggingFace | Meta Llama-3.2"],),
             "APIkey": ("STRING", {"default": "", "multiline": False, "tooltip": "Chatbot API"}),
-            "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff, "tooltip": "The random seed used for creating the noise."}),
-            "prompt": ("STRING", {"default": "", "multiline": True, "tooltip": "Chatbot prompt"})
+            "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff, "tooltip": "The random seed"}),
+            "main_prompt": ("STRING", {"default": "", "multiline": True, "tooltip": "Chatbot prompt"}),
+            "sub_prompt": ("STRING", {"default": "", "multiline": True, "tooltip": "Chatbot prompt"})
         }
         }
 
@@ -18,20 +20,41 @@ class API_chatbot:
     RETURN_TYPES = ("STRING",)
     FUNCTION = "api_chatbot"
 
-    def api_chatbot(self, chatbot, APIkey, seed, prompt):
+    def api_chatbot(self, chatbot, APIkey, seed, main_prompt, sub_prompt):
         model_list = {
             "Gemini 1.5 Flash": "gemini-1.5-flash",
-            "Gemini 1.5 Pro": "gemini-1.5-pro"
+            "Gemini 1.5 Pro": "gemini-1.5-pro",
+            "HuggingFace | Meta Llama-3.2": "meta-llama/Llama-3.2-3B-Instruct"
         }
         if "DPRandomGenerator" in ALL_NODE_CLASS_MAPPINGS:
             cls = ALL_NODE_CLASS_MAPPINGS["DPRandomGenerator"]
-            prompt = cls().get_prompt(prompt, seed, 'No')[0]
+            main_prompt = cls().get_prompt(main_prompt, seed, 'No')[0]
+            sub_prompt = cls().get_prompt(sub_prompt, seed, 'No')[0]
+        prompt = f"{main_prompt}.{sub_prompt}"
         model_name = model_list[chatbot]
-        if 'gemini' in model_name:
+        if 'Gemini' in chatbot:
             genai.configure(api_key=APIkey)
             model = genai.GenerativeModel(model_name)
             response = model.generate_content(prompt)
-            return (response.text.strip(),)
+            answer = response.text
+        if "HuggingFace" in chatbot:
+            answer = ""
+            client = OpenAI(
+                base_url="https://api-inference.huggingface.co/v1/", api_key=APIkey)
+            messages = [
+                {"role": "assistant", "content": prompt}
+            ]
+            stream = client.chat.completions.create(
+                model=model_name,
+                messages=messages,
+                temperature=0.5,
+                max_tokens=2048,
+                top_p=0.7,
+                stream=True
+            )
+            for chunk in stream:
+                answer += chunk.choices[0].delta.content
+        return (answer.strip(),)
 
 
 NODE_CLASS_MAPPINGS = {
