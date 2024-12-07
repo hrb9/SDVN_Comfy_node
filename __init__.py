744572c72491822449b6f03c56840c69c30ca0a2
phamhungd <phamhung.d@gmail.com>
2024-12-07 17:05:07 +0700

    Update auto install pip

--- __init__.py
+++ __init__.py
@@ -3,6 +3,39 @@ from .node.merge import NODE_CLASS_MAPPINGS as merge_node, NODE_DISPLAY_NAME_MAP
 from .node.creative import NODE_CLASS_MAPPINGS as creative_node, NODE_DISPLAY_NAME_MAPPINGS as creative_dis
 from .node.chatbot import NODE_CLASS_MAPPINGS as chatbot_node, NODE_DISPLAY_NAME_MAPPINGS as chatbot_dis
 
+import subprocess
+import sys, os
+
+def check_pip(package_name):
+    try:
+        result = subprocess.run(
+            [sys.executable, "-m", "pip", "show", package_name],
+            check=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        return True
+    except subprocess.CalledProcessError as e:
+        return False
+    
+def install():
+    list_check = os.path.join(os.path.dirname(__file__),"requirements.txt")
+    with open(list_check, 'r', encoding='utf-8') as file:
+        list_package = file.read().splitlines()
+
+    for package_name in list_package:
+        if "aria" in package_name and os.uname().sysname == "Darwin" and "#" not in package_name:
+            print(f"\033[{31}m{'Check SDVN-Comfy-Node: If your Mac doesn t have aria2 installed, install it via brew'}\033[0m")
+        else:
+            if check_pip(package_name):
+                print(f"Check SDVN-Comfy-Node: Package '{package_name}' is already installed.")
+            else:
+                print(f"Check SDVN-Comfy-Node: Package '{package_name}' not found. Installing...")
+                subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+
+install()
+
 NODE_CLASS_MAPPINGS = {
     **load_node,
     **merge_node,
@@ -17,3 +50,4 @@ NODE_DISPLAY_NAME_MAPPINGS = {
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+
