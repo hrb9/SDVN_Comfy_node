c4b438ac2fc0e290ed81251a2f5381595b071794
phamhungd <39170814+phamhungd@users.noreply.github.com>
2025-01-03 22:21:38 +0700

    update auto generate

--- node/preset.py
+++ node/preset.py
@@ -12,6 +12,18 @@ class AnyType(str):
 
 
 any = AnyType("*")
+def check_type_model(model):
+    for key in model.get_key_patches():
+        if "double_blocks.18" in key:
+            type_model = "Flux"
+            break
+        if "input_blocks.11" in key:
+            type_model = "SD 1.5"
+            break
+        if "transformer_blocks.1" in key:
+            type_model = "SDXL"
+            break
+    return type_model
 
 class quick_menu:
     @classmethod
@@ -38,6 +50,36 @@ class quick_menu:
     def quick_menu(s, Checkpoint, Lora, Lora2, Lora3, Lora4, Lora5, SimpleString, SimpleString2, String, String2):
         return (Checkpoint, Lora, Lora2, Lora3, Lora4, Lora5, SimpleString, SimpleString2, String, String2,)
 
+class load_model:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "Checkpoint":(folder_paths.get_filename_list("checkpoints"),),
+                "Lora":(["None",*folder_paths.get_filename_list("loras")],),
+                "Lora2":(["None",*folder_paths.get_filename_list("loras")],),
+                "Lora3":(["None",*folder_paths.get_filename_list("loras")],),
+                "Lora4":(["None",*folder_paths.get_filename_list("loras")],),
+                "Lora5":(["None",*folder_paths.get_filename_list("loras")],),
+                "Lora_Strength": ("STRING", {"default": "1,1,1,1,1", "multiline": False},),
+            }
+            }
+    CATEGORY = "📂 SDVN/✨ Preset"
+    RETURN_TYPES = ("MODEL", "CLIP", "VAE",)
+    RETURN_NAMES = ("model", "clip", "vae")
+    FUNCTION = "auto_generate"
+    def auto_generate(s, Checkpoint, Lora, Lora2, Lora3, Lora4, Lora5, Lora_Strength):
+        model, clip, vae = ALL_NODE["CheckpointLoaderSimple"]().load_checkpoint(Checkpoint)
+        list_lora = [Lora, Lora2, Lora3, Lora4, Lora5]
+        Lora_Strength = ALL_NODE["SDVN Simple Any Input"]().simple_any(Lora_Strength)[0]
+        for index in range(len(list_lora)):
+            if list_lora[index] != "None":
+                try:
+                    model, clip, _ = ALL_NODE["SDVN Load Lora"]().load_lora(False, "", "", list_lora[index], model, clip,  Lora_Strength[index] if index + 1 <= len(Lora_Strength) else Lora_Strength[-1], 1)["result"]
+                except:
+                    model, clip, _ = ALL_NODE["SDVN Load Lora"]().load_lora(False, "", "", list_lora[index], model, clip,  Lora_Strength[index] if index +1 <= len(Lora_Strength) else Lora_Strength[-1], 1)
+        return (model, clip, vae,)
+    
 class join_parameter:
     @classmethod
     def INPUT_TYPES(s):
@@ -55,9 +97,8 @@ class join_parameter:
                 "any10": ("PARAMETER",),
             }
         }
-    # INPUT_IS_LIST = True
-    # OUTPUT_IS_LIST = (True, )
-    CATEGORY = "📂 SDVN/💡 Creative"
+
+    CATEGORY = "📂 SDVN/✨ Preset"
     RETURN_TYPES = ("PARAMETER",)
     RETURN_NAMES = ("parameter",)
     FUNCTION = "join_parameter"
@@ -71,18 +112,14 @@ class join_parameter:
                 else:
                     r += [i]
         return (r,)
-class auto_generate_adv:
+class auto_generate:
     @classmethod
     def INPUT_TYPES(s):
         return {
             "required": {
-                "Checkpoint":(folder_paths.get_filename_list("checkpoints"),),
-                "Lora":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora2":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora3":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora4":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora5":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora_Strength": ("STRING", {"default": "1,1,1,1,1", "multiline": False},),
+                "Model":("MODEL",),
+                "Clip":("CLIP",),
+                "Vae":("VAE",),
                 "Prompt": ("STRING", {"default": "", "multiline": True},),
                 "Negative": ("STRING", {"default": "", "multiline": True, "placeholder": "No support Flux model"},),
                 "Active_prompt": ("STRING", {"default": "", "multiline": False},),
@@ -97,7 +134,6 @@ class auto_generate_adv:
                 "parameter": ("PARAMETER",)
             }
             }
-    # INPUT_IS_LIST = (False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True )
     CATEGORY = "📂 SDVN/✨ Preset"
     RETURN_TYPES = ("IMAGE",)
     RETURN_NAMES = ("image",)
@@ -109,22 +145,10 @@ class auto_generate_adv:
         "SDXL Hyper": [1024, "XL-BasePrompt", 0.3, 1536],
         "SD 1.5": [768, "1.5-BasePrompt", 0.4, 1920],
     }
-    def auto_generate(s, Checkpoint, Lora, Lora2, Lora3, Lora4, Lora5, Lora_Strength, Prompt, Negative, Active_prompt, Image_size, Steps, Denoise, seed, image = None, mask = None, parameter = None):
-        model, clip, vae = ALL_NODE["CheckpointLoaderSimple"]().load_checkpoint(Checkpoint)
-        list_lora = [Lora, Lora2, Lora3, Lora4, Lora5]
-        Lora_Strength = ALL_NODE["SDVN Simple Any Input"]().simple_any(Lora_Strength)[0]
-        for index in range(len(list_lora)):
-            if list_lora[index] != "None":
-                try:
-                    model, clip, _ = ALL_NODE["SDVN Load Lora"]().load_lora(False, "", "", list_lora[index], model, clip,  Lora_Strength[index] if index + 1 <= len(Lora_Strength) else Lora_Strength[-1], 1)["result"]
-                except:
-                    model, clip, _ = ALL_NODE["SDVN Load Lora"]().load_lora(False, "", "", list_lora[index], model, clip,  Lora_Strength[index] if index +1 <= len(Lora_Strength) else Lora_Strength[-1], 1)
-        if "flux" in Checkpoint.lower():
-            type_model = "Flux"
-        elif "xl" in Checkpoint.lower():
-            type_model = "SDXL"
-        else:
-            type_model = "SD 1.5"
+    def auto_generate(s, Model, Clip, Vae, Prompt, Negative, Active_prompt, Image_size, Steps, Denoise, seed, image = None, mask = None, parameter = None):
+        model, clip, vae = [Model, Clip, Vae]
+        type_model = check_type_model(model)
+        print(f"Type model : {type_model}")
         w, h = ALL_NODE["SDVN Simple Any Input"]().simple_any(Image_size)[0]
         Denoise = 1 if image == None else Denoise
         max_size = s.model_para[type_model][0] / Denoise
@@ -151,13 +175,7 @@ class auto_generate_adv:
                     p, n = ALL_NODE["SDVN Controlnet Apply"]().apply_controlnet(*para["controlnet"], vae=vae, positive = p, negative = n)["result"][:2]
                 if "applystyle" in para:
                     p = ALL_NODE["SDVN Apply Style Model"]().applystyle(*para["applystyle"], p)[0]
-        if type_model == "SDXL":
-            for i in list_lora:
-                if "lightning" in i.lower():
-                    type_model = "SDXL Lightning"
-                    break
-                if "hyper-sdxl" in i.lower():
-                    type_model = "SDXL Hyper"
+
         tile_size = s.model_para[type_model][3]
         _, img = ALL_NODE["SDVN KSampler"]().sample(model, p, type_model, "Denoise", "euler", "normal", seed, Tiled=True if (n_w > tile_size or n_h > tile_size) and Denoise < 0.5 else False, tile_width=int(round(n_w/2)), tile_height=int(round(n_h/2)), steps=Steps, cfg=7, denoise=Denoise, negative=n, latent_image=latent, vae=vae, FluxGuidance = 3.5)
         if w == n_w:
@@ -168,50 +186,17 @@ class auto_generate_adv:
             img = ALL_NODE["SDVN KSampler"]().sample(model, p, type_model, "Denoise", "euler", "normal", seed,  Tiled=True if (n_w > tile_size or n_h > tile_size) else False, tile_width=int(round(w/2)), tile_height=int(round(h/2)), steps=Steps, cfg=7, denoise=s.model_para[type_model][2], negative=n, latent_image=latent, vae=vae, FluxGuidance = 3.5)[1]
             return (img,)
         
-class auto_generate:
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "Checkpoint":(folder_paths.get_filename_list("checkpoints"),),
-                "Lora":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora2":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora3":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora4":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora5":(["None",*folder_paths.get_filename_list("loras")],),
-                "Lora_Strength": ("STRING", {"default": "1,1,1,1,1", "multiline": False},),
-                "Prompt": ("STRING", {"default": "", "multiline": True},),
-                "Active_prompt": ("STRING", {"default": "", "multiline": False},),
-                "Image_size": ("STRING", {"default": "1024,1024", "multiline": False},),
-                "Steps": ("INT", {"default": 20,},),
-                "Denoise": ("FLOAT", {"default": 1,"min":0,"max":1},),
-                "seed": ("INT", {"default": 0, "min": 0, "max": 0xffffffffffffffff, "tooltip": "The random seed"}),
-            },
-            "optional": {
-                "image": ("IMAGE",),
-                "mask": ("MASK",)
-            }
-            }
-
-    CATEGORY = "📂 SDVN/✨ Preset"
-    RETURN_TYPES = ("IMAGE",)
-    RETURN_NAMES = ("image",)
-    FUNCTION = "auto_generate"
-
-    def auto_generate(s, Checkpoint, Lora, Lora2, Lora3, Lora4, Lora5, Lora_Strength, Prompt, Active_prompt, Image_size, Steps, Denoise, seed, image = None, mask = None):
-        r = auto_generate_adv().auto_generate(Checkpoint, Lora, Lora2, Lora3, Lora4, Lora5, Lora_Strength, Prompt, "", Active_prompt, Image_size, Steps, Denoise, seed, image, mask)[0]
-        return (r,)
                 
 NODE_CLASS_MAPPINGS = {
     "SDVN Quick Menu": quick_menu,
     "SDVN Auto Generate": auto_generate,
-    "SDVN Auto Generate Advance": auto_generate_adv,
     "SDVN Join Parameter": join_parameter,
+    "SDVN Load Model": load_model,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "SDVN Quick Menu": "📋 Quick Menu",
     "SDVN Auto Generate": "💡 Auto Generate",
-    "SDVN Auto Generate Advance": "💡 Auto Generate Advance",
     "SDVN Join Parameter": "🔄 Join Parameter",
+    "SDVN Load Model": "💿 Load Model"
 }
